5f21d0a98ab8123314cf0c1a65c2110bdd3fc73b
rickyjasso <rickyjassoa@gmail.com>
2023-11-13 10:51:44 -0600

    styled 'nosotros' section

--- src/scenes/nosotros/index.tsx
+++ src/scenes/nosotros/index.tsx
@@ -1,8 +1,9 @@
-import useMediaQuery from "@/hooks/useMediaQuery";
-import HText from "@/shared/HText";
+
 import { SelectedPage } from "@/shared/types"
 import { motion } from "framer-motion";
-
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -10,40 +11,53 @@ type Props = {
 
 const Nosotros = ({setSelectedPage}: Props) => {
 
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isActive, setIsActive] = useState(false);
+
+  const setFalse = () => {
+      setIsActive(false);
+    }
+    const setTrue = () => {
+      setIsActive(true);
+    }
 
   return (
-    <section id="nosotros" className="mx-auto w-5/6 pt-12 pb-16">
-      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Nosotros)}>
-        {
-        isAboveMediumScreens? <div>
-          <div className="mb-10 text-center"><HText textSize="text-3xl"><span className="text-molse-primary">Nosotros</span></HText></div>
-          <div className="flex w-50 justify-center align-middle">
-            <div className="flex-row">
-                <h1 className="basis-3/5 font-montserrat text-2xl font-bold mt-5 mb-1 text-molse-tertiary">Trayectoria</h1>
-                <p>MOLSE ABOGADOS, se constituye y da inicio a sus operaciones en el año 2008 con el fin de dar asesorar a clientes en las ramas del derecho mercantil, civil y familiar; posteriormente en el año 2020 se consolida y da apertura a un nuevo servicio profesional en la rama del Derecho Laboral.
-</p>
-                <h1 className="basis-3/5 font-montserrat text-2xl font-bold mt-5 mb-1 text-molse-tertiary">Visión</h1>
-                <p>Ser una de las firmas legales líderes en el Norte de México, brindando servicios íntegros, transparencia y de calidad a nuestros clientes.</p>              
-            </div>
+    <section id="nosotros" className="mx-auto sm:w-5/6 pt-12 pb-32 text-black">
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Nosotros)} className="md:flex relative z-10">
+        <div className="md:absolute z-20">
+          <div>
             <div>
-            <h1 className="basis-3/5 font-montserrat text-2xl font-bold mt-5 mb-1 text-molse-tertiary">Valores</h1>
-            <p>Lealtad, Responsabilidad, Disciplina, Tenacidad.</p>
-
+              <button onClick={setFalse} className={isActive ? 'w-2/5 md:w-1/5 py-2 bg-slate-100' : 'border-t-2 border-molse-primary bg-white w-2/5 md:w-1/5 py-2'}>Trayectoria</button>
+              <button onClick={setTrue} className={isActive ? 'border-t-2 border-molse-primary bg-white w-2/5 md:w-1/5 py-2' : 'w-2/5 md:w-1/5 py-2 bg-slate-100'}>Nosotros</button>
             </div>
           </div>
-        </div>: 
-        (<div>
-          <div className="mb-10"><HText textSize="3xl"><span className="text-molse-primary">Nosotros</span></HText></div>
-          <h1 className="basis-3/5 font-montserrat text-2xl font-bold mt-5 mb-1 text-molse-tertiary">Trayectoria</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores error distinctio placeat.</p>
-          <h1 className="basis-3/5 font-montserrat text-2xl font-bold mt-5 mb-1 text-molse-tertiary">Visión</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci error odio harum provident deserunt inventore odit facilis necessitatibus maxime illum.</p>
-          <h1 className="basis-3/5 font-montserrat text-2xl font-bold mt-5 mb-1 text-molse-tertiary">Valores</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aperiam quidem dolor facere? In, consequatur?</p>
-        </div>)
-        }
-        
+          <div>
+            <div className="bg-white md:w-3/5 drop-shadow-xl py-5 px-10 text-lg">
+            <div className={isActive ? 'hidden' : 'block my-5'}>
+                <h1 className="mb-3 text-3xl text-molse-primary font-bold">Trayectoria</h1>
+                <p>MOLSE ABOGADOS, se constituye y da inicio a sus operaciones en el año 2008 con el fin de dar asesorar a clientes en las ramas del derecho mercantil, civil y familiar; posteriormente en el año 2020 se consolida y da apertura a un nuevo servicio profesional en la rama del Derecho Laboral.</p>
+                <img src="" alt="" />
+              </div>
+              <div className={isActive ? 'block my-5' : 'hidden'}>
+                <div className="my-5">
+                  <h1 className="mb-3 text-3xl text-molse-primary font-bold">Nuestra Visión</h1>
+                  <p>Ser una de las firmas legales líderes en el Norte de México, brindando servicios íntegros, transparencia y de calidad a nuestros clientes.</p>
+                </div>
+                <div className="my-5">
+                  <h1 className="mb-3 text-3xl text-molse-primary font-bold">Nuestros Valores</h1>
+                  <ul>
+                      <li className="flex my-5"><span><FontAwesomeIcon className="mr-2 text-molse-primary" icon={faCheck}/></span>Lealtad</li>
+                      <li className="flex my-5"><span><FontAwesomeIcon className="mr-2 text-molse-primary" icon={faCheck}/></span>Responsabilidad</li>
+                      <li className="flex my-5"><span><FontAwesomeIcon className="mr-2 text-molse-primary" icon={faCheck}/></span>Disciplina</li>
+                      <li className="flex my-5"><span><FontAwesomeIcon className="mr-2 text-molse-primary" icon={faCheck}/></span>Tenacidad</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex md:justify-end md:w-full justify-center">
+          <img className="" src="https://brujuladigital.net/images/news/es/b_24516_bolivianos-de-experiencia-y-trayectoria-en-el-sector-empresarial-dan-vida-a-braq-empresa-de-servicios-corporativos-1.jpeg" alt="" />
+        </div>
       </motion.div>
     </section>
   )
