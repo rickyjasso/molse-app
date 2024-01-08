2e3dfa777133de039b435f29a6cd7f007894d832
rickyjasso <rickyjassoa@gmail.com>
2024-01-08 11:06:48 -0600

    logo

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
-import Logo from "@/assets/Logo-transparent.png"
+import Logo from "/assets/Logo-transparent.png"
 import Link from "./Link"
 import { SelectedPage } from "@/shared/types"
 import useMediaQuery from "@/hooks/useMediaQuery"
