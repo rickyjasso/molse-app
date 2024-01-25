aa748be3e3d90ebb634a8eec68ee489065d9ab91
rickyjasso <rickyjassoa@gmail.com>
2024-01-25 15:14:36 -0600

    error istop

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,18 @@ import { SelectedPage } from "@/shared/types";
 function App() {
 
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
-  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
-        setIsTopOfPage(true);
         setSelectedPage(SelectedPage.Home);
       }
-      if (window.scrollY !== 0) setIsTopOfPage(false);
     }
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
     <div className="app bg--white font-timesnewroman">
-      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
       <Home setSelectedPage={setSelectedPage}></Home>
       <Servicios setSelectedPage={setSelectedPage}></Servicios>
       <Nosotros setSelectedPage={setSelectedPage}></Nosotros>
