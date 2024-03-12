300e5e7c2ba016e1a6ce84dcd9c73f263ff6c671
rickyjasso <rickyjassoa@gmail.com>
2024-03-12 13:03:57 -0600

    changes to services, missing eng translations for 03, 04

--- src/scenes/servicios/index.tsx
+++ src/scenes/servicios/index.tsx
@@ -36,8 +36,8 @@ const Servicios = ({setSelectedPage}: Props) => {
       {/* <section id="services"></section> */}
     <section id="servicios" className="mx-auto w-5/6">
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Servicios)}>
-        <div className="flex-row justify-center align-middle text-start">
-          <HText textSize="text-3xl"> <span className="text-molse-primary"> {t('ourServices')} </span></HText>
+        <div className="flex-row justify-center align-middle text-start md:mb-20">
+          <HText textSize="text-3xl"> <span className="text-molse-primary font-bold"> {t('ourServices')} </span></HText>
         </div>
         <div className="flex md:my-3 md:flex-wrap flex-col my-6">
           {servicesElements}
--- src/shared/Card.tsx
+++ src/shared/Card.tsx
@@ -10,16 +10,16 @@ const Card = ({ title, body, id, icon }: Props) => {
 
   return (
     <div className="relative">
-      <div className="group flex-col mx-3 my-5 p-6 bg-white border rounded-xl shadow border-gray-700 md:hover:bg-molse-primary h-full flex items-center md:hover:scale-105 transition-all">
-        {icon && <img src={icon} alt={`${title} Icon`} className="w-12 h-12 mb-4" />}
-        <div className="flex items-center align-middle relative">
-          <h3 className="absolute left-1/2 text-7xl transform -translate-x-1/2 font-bold opacity-10 text-molse-primary md:group-hover:text-white">
+      <div className="group flex-col mx-3 my-5 p-6 bg-white shadow flex transition-all items-center h-auto">
+        {icon && <img src={icon} alt={`${title} Icon`} className="w-16 h-16 mb-4 md:self-end" />}
+        <div className="flex align-middle items-center relative mb-6">
+          <h3 className="absolute left-1/2 text-9xl transform -translate-x-1/2  font-bold opacity-30 text-molse-secondary ">
             0{id}
           </h3>
-          <h5 className="my-3 text-xl font-bold tracking-tight text-black md:group-hover:text-white relative z-10">{title}</h5>
+          <h5 className="my-3 md:text-4xl text-3xl font-bold text-center tracking-wide text-molse-primary z-10">{title}</h5>
         </div>
         {bodyArray.map((paragraph, index) => (
-          <p key={index} className="my-5 font-normal text-base text-gray-600 md:group-hover:text-white text-center relative z-10">
+          <p key={index} className="my-5 tracking-widest font-normal md:text-lg text-base text-molse-secondary md:text-justify text-center relative z-10">
             {paragraph}
           </p>
         ))}
